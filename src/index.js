a043a5c2ca9065046d0ab9c7e18c32c20ad31d8a
Dayana00 <70838027+Dayana00@users.noreply.github.com>
2021-03-08 15:04:10 +0300

    fix: change index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minArray;
+    if ( array == null || array.length === 0 ) {
+        return 0;
+    } else {
+        minArray = Math.min.apply ( null, array);
+        return minArray;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxArray;
+    if ( array == null || array.length === 0) {
+        return 0;
+    } else {
+        let maxArray = Math.max.apply ( null, array);
+        return maxArray;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avgArray;
+    let sum = 0;
+    if ( array == null || array.length === 0 ) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++){
+            sum +=array[i];
+        }
+        avgArray = sum / array.length;
+        return avgArray;
+    }
 }
